e8b0701371692405f88528ad46e6755e8ee00df2
Henok <henokasegdew110@gmail.com>
2024-11-29 15:45:41 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist', // Output directory for build files
+  },
   plugins: [react()],
   server: {
     port: 3000,
